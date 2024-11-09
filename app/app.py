63cd47725b7336f2c0ddec4642f1ada9d5e39d73
olgatorres <bestlaly@mail.ru>
2024-11-09 21:39:57 +0300

    add edf save full

--- app/app.py
+++ app/app.py
@@ -5,20 +5,18 @@ from datetime import datetime
 import plotly.graph_objs as go
 from plotly.io import to_html
 from contextlib import asynccontextmanager
-from matplotlib import pyplot as plt
 from plotly.subplots import make_subplots
 
 from reportlab.pdfgen import canvas
-from fastapi import FastAPI, Request, UploadFile, HTTPException, Query
+from fastapi import FastAPI, Request, UploadFile, HTTPException, Query, Response
 from pyedflib import EdfReader
 from starlette.responses import HTMLResponse, FileResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-from config import files_path, error_msg, plot_path, zoomed_plot_path, plot_height, plot_width
+from config import files_path, error_msg, plot_path, zoomed_plot_path, plot_height, plot_width, public_path
 
 sampling_rate, edf_data, n_signals, signal_labels = 0, [], 0, []
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -59,26 +57,20 @@ def get_prediction(data):
     pass
 
 
-def preprocessing(file_location: str):
-    """Обработка данных для отправки в модель."""
-    pass
-
-
-# делаем кнопку для сохранения в файл xls, edf
 # делаем кнопку для вывода периодов
 # в самом графике должна быть разметка по аннотациям
 # если можно ли сделать вывод периодов - то делаем функцию show_period и кнопку для нее
-
+# переделать на MNE
 @app.post("/upload/", tags=["results of check"], response_class=HTMLResponse)
 async def upload_file(request: Request, file: UploadFile):
     global sampling_rate, edf_data, n_signals, signal_labels
+    file_name = file.filename
+    file_location = os.path.join(files_path, 'test') # тут должно быть норм имя файла
 
-    file_location = f"temp_{file.filename}"
     with open(file_location, "wb") as buffer:
         buffer.write(await file.read())
     try:
-        data = preprocessing(file_location)  # TO DO
-        predict = get_prediction(data)  # TO DO
+        predict = get_prediction(file_location)  # TO DO
 
         edf_reader = EdfReader(file_location)
         n_signals = edf_reader.signals_in_file
@@ -90,22 +82,25 @@ async def upload_file(request: Request, file: UploadFile):
         graph_html = fig.to_html(full_html=False)
         fig.write_image(plot_path)
         edf_reader.close()
-        os.remove(file_location)
+        # os.remove(file_location)
         id, age, pharm, period = _get_rat_data(str(file.filename))
 
         return templates.TemplateResponse("result.html",
                                           {"request": request,
                                            "id": id, "age": age, "pharm": pharm, "period": period,
+                                           "file": file.filename,
                                            "graph_html": graph_html})
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"{error_msg}: {str(e)}")
 
 
-# в самом графике должна быть разметка по аннотациям
+# в самом графике должна быть разметка по аннотациям,
+# надо сюда прокинуть имя файла, чтобы выводилось в пдф
 @app.get("/generate-pdf", tags=["load pdf file with plots"])
 async def generate_pdf(is_zoomed: bool = Query(False), start_time: float = None, end_time: float = None):
     """Генерация файла pdf из изначального графика и увеличенных версий"""
     global sampling_rate, edf_data, n_signals, signal_labels
+    file_name = '' # имя файла, чтобы выводилось в пдф
 
     plots_list = [plot_path]
     if is_zoomed and start_time is not None and end_time is not None:
@@ -114,35 +109,41 @@ async def generate_pdf(is_zoomed: bool = Query(False), start_time: float = None,
         fig.write_image(zoomed_plot_path)
         plots_list.append(zoomed_plot_path)
 
-    pdf_path = save_to_pdf(plots_list, f"Plot_{datetime.now().strftime('%Y-%m-%d-%H-%M')}")
+    pdf_path = save_to_pdf(plots_list, file_name, f"Plot_{datetime.now().strftime('%Y-%m-%d-%H-%M')}")
     return FileResponse(pdf_path, media_type='application/pdf', filename=os.path.basename(pdf_path))
 
 
-# buttom in html сделать
-def save_to_edf(filename: str) -> str:
-    edf_path = f"{filename}.edf"
-    buffer = io.BytesIO()
-    buffer.seek(0)
-    with open(edf_path, "wb") as f:
-        f.write(buffer.read())
-    return edf_path
+# тут надо прокинуть нормальное имя файла, а не тестовое
+@app.get("/generate-edf", tags=["load edf file with predictions"])
+def save_to_edf(file_name='test'):
+    """Сохранение полученной разметки в EDF"""
+    file_location = os.path.join(files_path, file_name)
+    if not os.path.exists(file_location):
+        return {"error": f"Файл {file_name} не найден."}
+
+    return Response(
+        content=open(file_location, 'rb').read(),
+        media_type="application/octet-stream",
+        headers={
+            "Content-Disposition": f"attachment; filename={os.path.basename(file_name)}"
+        }
+    )
 
 
-def save_to_pdf(image_paths: list, filename: str) -> str:
+def save_to_pdf(image_paths: list, title: str, filename: str) -> str:
     """Сохранение изображений графиков в PDF"""
     pdf_path = f"{filename}.pdf"
     buffer = io.BytesIO()
     c = canvas.Canvas(buffer)
-    c.setFont("Helvetica", 16)
-    c.drawString(10, 605, "PDF.")
+    c.setFont("Helvetica", 20)
 
-    for img_path in image_paths:
-        c.drawImage(img_path, 10, 600, width=plot_height, height=plot_width)
+    for img_path in image_paths: # тут размеры х, у, ширина, высота
+        c.drawString(100, 650, title) # тут название файла и может еще инфо
+        c.drawImage(img_path, 10, 500, width=plot_height, height=plot_width)
         c.showPage()
 
     c.save()
     buffer.seek(0)
-    # Сохраняем PDF на диск
     with open(pdf_path, "wb") as f:
         f.write(buffer.read())
     return pdf_path
@@ -158,7 +159,7 @@ def _get_rat_data(file_name: str):
 @app.get("/publications/", response_class=HTMLResponse)
 async def publications_list(request: Request):
     """Show links to publications"""
-    publications = [f.name for f in files_path.glob("*.pdf")]
+    publications = [f.name for f in public_path.glob("*.pdf")]
     return templates.TemplateResponse("publications.html",
                                       {"request": request, "publications": publications})
 
--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 STORAGE = 'storage'
-files_path = Path("storage")
+public_path = Path("storage/publications")
+files_path = Path("storage/archive")
 error_msg = 'Что-то пошло не так, мы работаем над этим'
 plot_path = "static/plot.png"
 zoomed_plot_path = "static/plot_zoomed.png"
 plot_height = 500
 plot_width = 220
-model_path = 'model/..'
+model_path = 'model/model.h5'
